f1db5fe7bc690f526cfc2531d29f92603ad9c256
hagemanto-saitama <yuki.hyodo0201@gmail.com>
2022-09-27 16:11:04 +0900

    add:client-api通信テスト追加

---
+++ src/ukiukiwork/client/pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+
+<script>
+  export default {
+    async asyncData ({ app }) {
+      const data = await app.$axios.$get(`http://localhost/api`)
+      return { data }
+    }
+  }
+</script>
